713caa4f93336cb3fed509f8ab3c1e7c28598db8
Antonio Carpentieri <a.carpe@gmail.com>
2017-03-24 00:27:10 +0100

    Added export in files

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ Support = require('./backbone-support/support.js');
 Support.Observer = require('./backbone-support/observer.js');
 Support.CompositeView = require('./backbone-support/composite_view.js');
 Support.SwappingRouter = require('./backbone-support/swapping_router.js');
+module.exports = Support;
