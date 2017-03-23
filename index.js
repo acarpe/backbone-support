1968ff1b90ae24ebfcd630996ef8e19407fb39ae
Antonio Carpentieri <a.carpe@gmail.com>
2017-03-23 23:08:00 +0100

    Fixed syntax

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require 'backbone-support/support.js'
-require 'backbone-support/observer.js'
-require 'backbone-support/composite_view.js'
-require 'backbone-support/swapping_router.js'
+require('backbone-support/support.js');
+require('backbone-support/observer.js');
+require('backbone-support/composite_view.js');
+require('backbone-support/swapping_router.js');
